7dcfbf199a57c2449f50d45c10b35c87380f0a0d
Rohit Balage <thatcomputerboy26@gmail.com>
2024-04-21 17:16:37 -0400

    updated UI with Viewmodel

--- app/src/main/java/com/rrbofficial/mvvmcities/view/MainActivity.kt
+++ app/src/main/java/com/rrbofficial/mvvmcities/view/MainActivity.kt
@@ -1,21 +1,43 @@
 package com.rrbofficial.mvvmcities.view
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
 import com.rrbofficial.mvvmcities.R
+import com.rrbofficial.mvvmcities.databinding.ActivityMainBinding
+import com.rrbofficial.mvvmcities.model.City
+import com.rrbofficial.mvvmcities.viewmodel.CityViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val model: CityViewModel by viewModels()
+    private  lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+       model.getCityData().observe(this, Observer { city ->
+
+           binding.cityImage.setImageDrawable(
+               ResourcesCompat.getDrawable(resources, city.img, applicationContext.theme)
+           )
+
+           binding.cityNameTV.text = city.name
+           binding.cityPopulationTV.text = city.population.toString()
+       })
     }
 }
